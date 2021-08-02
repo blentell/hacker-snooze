df006e68a6ea47bf64fc779a7752c16bd31fa5d6
blentell <blentell@aol.com>
2021-08-02 14:24:37 -0400

    Update index.html, styles.css and main.js with working code

---
+++ main.js
@@ -0,0 +1,57 @@
+// Example adding two elements to the DOM
+
+const body = document.querySelector("#main");
+
+// const parent = document.createElement("div");
+// parent.className = "parent";
+// parent.innerText = "Bacon may disappear.";
+// const child = document.createElement("div");
+// child.className = "child";
+// child.innerText = "1 point by woldermariam";
+// parent.appendChild(child);
+// body.appendChild(parent);
+
+// Result:
+//
+// <body>
+//     <div class="parent">
+//         <div class="child">
+//         </div>
+//     </div>
+// </body>
+
+async function makeApiRequest() {
+	const rawData = await fetch(
+		"https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+	);
+	const data = await rawData.json();
+
+	for (let i = 0; i < 100; i++) {
+		await fetch(
+			`https://hacker-news.firebaseio.com/v0/item/${data[i]}.json?print=pretty`
+		).then(async function (data) {
+			const newdata = await data.json();
+			const parent = document.createElement("div");
+			parent.className = "parent";
+			parent.innerHTML = `
+            <div class="card">
+                <div class="card-body">    
+                    <a class='parent1' href='${newdata.url}'target="_blank">${
+				i + 1
+			}.   ${newdata.title}
+                    </a>
+                </div>
+            </div>              
+                `;
+
+			const child = document.createElement("div");
+			child.className = "child";
+			child.innerText = `${newdata.score} points by ${newdata.by}
+            total comments ${newdata.descendants}`;
+			body.appendChild(parent);
+			parent.appendChild(child);
+		});
+	}
+}
+
+makeApiRequest();
